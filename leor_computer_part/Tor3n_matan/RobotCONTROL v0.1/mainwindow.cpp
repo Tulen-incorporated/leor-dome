f0212a9ef533d08fbc9022d84b218864614116ac
Vasilly.P <strange.snork@gmail.com>
2013-02-10 21:53:36 +0400

    Переделал все (

--- leor_computer_part/Tor3n_matan/RobotCONTROL v0.1/mainwindow.cpp
+++ leor_computer_part/Tor3n_matan/RobotCONTROL v0.1/mainwindow.cpp
@@ -106,12 +106,46 @@ void MainWindow::keyPressEvent(QKeyEvent *event)
         break;
 //......................................... захват +-
      } //switch
-
-    // Теперь обновялем лейблы.
 }
 
-void MainWindow::keyReleaseEvent(QKeyEvent *)
+
+void MainWindow::keyReleaseEvent(QKeyEvent *event)
 {
+    // Все тоже самое, но все дельты зануляем.
+    switch (event->key())
+    {
+    case Qt::Key_W:
+    case Qt::Key_S:
+        actualDeltas[0] = 0;
+        break;
+//......................................... верх низ основание
+
+    case Qt::Key_A:
+    case Qt::Key_D:
+        actualDeltas[1] = 0;
+        break;
+//......................................... право лево основание
+    case Qt::Key_Up:
+    case Qt::Key_Down:
+        actualDeltas[2] = 0;
+        break;
+//......................................... верх низ локоть
+    case Qt::Key_PageUp:
+    case Qt::Key_PageDown:
+        actualDeltas[3] = 0;
+        break;
+//......................................... кисть верх низ
+    case Qt::Key_Left:
+    case Qt::Key_Right:
+        actualDeltas[4] = 0;
+        break;
+//......................................... кисть левоправо
+    case Qt::Key_Space:
+    case Qt::Key_X:
+        actualDeltas[5] = 0;
+        break;
+//......................................... захват +-
+     } //switch
 }
 
 // Слот для таймера, тут все просто. Как только таймер переполняется, меняем углы на текущие дельты.
@@ -123,4 +157,6 @@ void MainWindow::timerSlot()
     {
         angles[i] += actualDeltas[i];
     }
+
+    //TODO я забыл обновить лейблы (
 }
